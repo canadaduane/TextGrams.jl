306c9a782e9dc4bdee3ae6fcd145c67e911a8121
Duane Johnson <duane@instructure.com>
2015-03-20 08:40:42 -0600

    Update comparison output, add threshold for words

--- compare.jl
+++ compare.jl
@@ -20,9 +20,15 @@ function parse_commandline()
             nargs = 1
         "--ngrams", "-n"
             help = "the N in ngrams (e.g. '3' to create up to 3-grams)"
-            nargs = 1
             arg_type = Integer
-            default = Integer[2]
+            default = 2
+        "--show-matches", "-s"
+            help = "show matching words with score above the threshold"
+            action = :store_true
+        "--threshold", "-t"
+            help = "set the 'show-matches' threshold. Defaults to 0.25."
+            arg_type = Float64
+            default = 0.25
         "FILES"
             help = "files or directories to cross compare, use 'X' to separate list A from list B"
             required = true
@@ -67,52 +73,74 @@ msg("Using files X: ", fileX)
 msg("Using files Y: ", fileY)
 
 function cross(fn, fileX, fileY)
+  x = 1
   for fx in @task(fileProducer(fileX))
+    y = 1
     for fy in @task(fileProducer(fileY))
-      fn(fx, fy)
+      if x <= y && fx != fy
+        fn(fx, fy)
+      end
+      y += 1
     end
+    x += 1
   end
 end
 
-function loadAndCompare(baseline, fx, fy)
-  # try
-  # msg("Loading $(fx)...")
-  nx = Ngrams(Document(open(fx)),2)
-  # msg("Loading $(fy)...")
-  ny = Ngrams(Document(open(fy)), 2)
-  # msg("Comparing...")
-  compare(baseline, nx, ny)
-  # catch e
-    # println("Skipping ", fx, " X ", fy, " (Not an ASCII file?)")
-    # 0.0
-  # end
+function loadAndCompare(baseline, fx, fy, show_matches_threshold=0.25)
+  try
+    msg("Loading $(fx)...")
+    nx = Ngrams(Document(open(fx)),2)
+    msg("Loading $(fy)...")
+    ny = Ngrams(Document(open(fy)), 2)
+    msg("Comparing... $(fx) x $(fy)")
+    compare(baseline, nx, ny, 4, fx, fy, show_matches_threshold)
+  catch e
+    println("Skipping ", fx, " X ", fy, " (Not an ASCII file?)")
+    0.0
+  end
 end
 
-function compare(baseline, nx, ny)
-  total = 0
+function compare(baseline, nx, ny, unknownfreq=4, fx=None, fy=None, show_matches_threshold=0.25)
+  total_inv = 0
+  total_mul = 0
+  total_sqr = 0
   sx = int(sum(values(nx))/2)
   sy = int(sum(values(ny))/2)
 
-  if sx < sy
-    for (ngram, freqx) in nx
-      freqy = get(ny, ngram, 0)
-      freqbase = get(baseline, ngram, 10000000)
-      interscore = sqrt(freqx * freqy) / freqbase
-      total += interscore
-    # if interscore >= 1
-      # println(ngram, " ", freqbase, ":", freqx, ",", freqy, " : ", interscore)
-    # end
-    end
-  else
-    for (ngram, freqy) in ny
-      freqx = get(nx, ngram, 0)
-      freqbase = get(baseline, ngram, 10000000)
-      interscore = sqrt(freqx * freqy) / freqbase
-      total += interscore
+  if sy < sx
+    sx, sy = sy, sx
+    nx, ny = ny, nx
+    fx, fy = fy, fx
+  end
+
+  saved_scores = Array(Any, 0)
+  for (ngram, freqx) in nx
+    freqy = get(ny, ngram, 0)
+    if freqx > 0 && freqy > 0
+      freqbase = get(baseline, ngram, unknownfreq)
+      interscore_inv = 1.0 / freqbase
+      interscore_mul = (freqx * freqy) / freqbase
+      interscore_sqr = sqrt(freqx * freqy) / freqbase
+      total_inv += interscore_inv
+      total_mul += interscore_mul
+      total_sqr += interscore_sqr
+      if interscore_inv >= show_matches_threshold
+        push!(saved_scores, (ngram, interscore_inv, interscore_mul, interscore_sqr))
+      end
     end
   end
 
-  (total / sqrt(sx^2 + sy^2), sx, sy)
+  nfx, _ = splitext(basename(fx))
+  nfy, _ = splitext(basename(fy))
+
+  if length(saved_scores) > 0
+    for parts = sort(saved_scores, by = (t) -> t[2], rev=true)
+      ngram, i_inv, i_mul, i_sqr = parts
+      @printf("\t%s\t%0.3f\t%0.3f\t%0.3f\t%s\t%s\n", ngram, i_inv, i_mul, i_sqr, nfx, nfy)
+    end
+  end
+
+  (total_inv, total_mul, total_sqr, sx, sy, nfx, nfy)
 end
 
 function linesInFile(filename)
@@ -123,11 +151,23 @@ end
 
 msg("Measuring baseline size...")
 linecount = linesInFile(baselinePath)
-p = Progress(linecount, 1, yellow("Loading baseline... "), 50)
-baseline = @time Ngrams(open(baselinePath), (n) -> next!(p))
+msg("  $(linecount) ngrams")
+if settings["verbose"]
+  p = Progress(linecount, 1, yellow("Loading baseline... "), 50)
+  baseline = @time Ngrams(open(baselinePath), (n) -> next!(p))
+else
+  baseline = Ngrams(open(baselinePath))
+end
 
 msg("\nCross comparing...")
+if !settings["show-matches"]
+  @printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "total_inv", "total_mul", "total_sqrt", "sizex", "sizey", "namex", "namey")
+end
 cross(fileX, fileY) do fx, fy
-  score, sizex, sizey = loadAndCompare(baseline, fx, fy)
-  println(score, "\t", sizex, "\t", sizey, "\t", fx, "\t", fy)
+  threshold = settings["show-matches"] ? settings["threshold"] : 1000000
+  total_inv, total_mul, total_sqr, sizex, sizey, nfx, nfy = loadAndCompare(baseline, fx, fy, threshold)
+  if settings["show-matches"]
+    @printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\n", "total_inv", "total_mul", "total_sqrt", "sizex", "sizey", "namex", "namey")
+  end
+  @printf("%0.5f\t%0.5f\t%0.5f\t%d\t%d\t%s\t%s\n", total_inv, total_mul, total_sqr, sizex, sizey, nfx, nfy)
 end
