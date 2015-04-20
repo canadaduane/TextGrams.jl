125107206093ae157a65dd60d7f57dc0dc624b9d
Duane Johnson <duane.johnson@gmail.com>
2015-04-19 21:09:45 -0600

    Forgot some files

--- src/Document.jl
+++ src/Document.jl
@@ -1,17 +1,23 @@
 using MutableStrings
 using Base
 
-export Document, copy
+export Document, copy, wordcount, splitDocument
 
 import Base.copy
 
 type Document
   content::MutableASCIIString
   title::String
-  function Document(c::MutableASCIIString, t = None)
-    clean!(c)
+  function Document(c::MutableASCIIString, t=None, cleanIt=true)
+    if cleanIt
+      clean!(c)
+    end
     new(c, t == None ? titleFromContent(c) : t)
   end
+end
+
+function Document(content::String, title=None, cleanIt=true)
+  Document(MutableASCIIString(content), title, cleanIt)
 end
 
 function Document(content::String, title=None)
@@ -31,4 +37,39 @@ end
 
 function copy(d::Document)
   Document(d.content, copy(d.title))
+end
+
+function wordcount(d::Document)
+  count(c -> c == ' ' || c == '\n', collect(d.content))
+end
+
+function splitDocument(d::Document, wordsPer::Integer)
+  index = 0
+  splitIndex = 1
+  prevSplitIndex = 0
+  documents = Document[]
+  while splitIndex > 0
+    splitIndex = searchNth(d.content, ' ', wordsPer, splitIndex + 1)
+    if splitIndex > 0
+      # middle chunk, split from prev to current index
+      push!(documents, Document(d.content[(prevSplitIndex+1):(splitIndex-1)], None, false))
+    else
+      # last chunk, split to end
+      push!(documents, Document(d.content[(prevSplitIndex+1):end], None, false))
+      break
+    end
+    prevSplitIndex = splitIndex
+  end
+  return documents
+end
+
+function searchNth(s::MutableASCIIString, chars, n::Integer, index = 0)
+  for i in range(1, n)
+    index = search(s, chars, index+1)
+    # shortcut if nothing found
+    if index == 0
+      break
+    end
+  end
+  return index
 end
